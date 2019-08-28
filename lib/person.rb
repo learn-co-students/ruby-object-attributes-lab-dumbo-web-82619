0e523932d34597bf407a73afef2f5cec7e65fd50
James Gibbel <jamesgibbel@gmail.com>
2019-08-28 13:21:20 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name 
+    @name
+  end 
+  def name=(name_entered)
+    @name = name_entered
+  end 
+  def job 
+    @job
+  end
+  def job=(job_entered)
+    @job = job_entered
+  end 
+end 
+
