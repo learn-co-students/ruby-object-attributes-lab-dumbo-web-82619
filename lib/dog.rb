9cce64489e92fc5c90aee5230782c2b74c0cee37
Franky Liu <cuifranky95@gmail.com>
2019-08-28 13:18:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,47 @@
+class Dog
+def initialize
+  @name = name
+end
+def name
+  @name
+end
+def name=(new_name)
+  @name= new_name
+end
+end
+
+class Dog
+def initialize
+  @breed = breed
+end
+def breed
+  @breed
+end
+def breed=(new_breed)
+  @breed= new_breed
+end
+end
+
+class Person
+  def initialize
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name= new_name
+  end
+end
+
+class Person
+  def initialize
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job= (new_job)
+    @job= new_job
+  end
+end
